daa2cb5df271aae81e9d6652cb57aa757ffd99ed
nithyayamsinghe <nitheeromeshi@gmail.com>
2022-05-17 16:16:11 +0530

    update end point

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,7 @@ module.exports.updateUser = async (userId, data) => {
   if (data.password) {
     data.password = CryptoJS.AES.encrypt(
       data.password,
-      process.env.JWT_SECRET
+      process.env.JWT_SEC
     ).toString();
   }
 
